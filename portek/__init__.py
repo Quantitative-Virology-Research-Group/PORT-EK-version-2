1d3b6303aa98186bf332568c1fc6ba267f30bcb0
wis-janusz <wis.janusz@gmail.com>
2025-03-06 13:04:24 +0100

    remove deprecated methods from enriched

--- portek/__init__.py
+++ portek/__init__.py
@@ -5,12 +5,6 @@ from .portek_utils import calc_kmer_pvalue
 from .portek_utils import assign_kmer_group_ava, assign_kmer_group_ovr
 from .portek_utils import check_exclusivity
 from .portek_utils import build_similarity_graph_two_list
-# from .portek_utils import calc_agg_freq
-# from .portek_utils import map_kmers_find_mutations
-# from .portek_utils import assemble_kmers
-# from .portek_utils import plot_segments
-# from .portek_utils import plot_kmers_by_genome
-# from .portek_utils import assign_gene_from_interval, assign_gene_from_position
 from .portek_utils import make_ordinal
 from .portek_utils import assemble_kmers_debruijn
 from .portek_utils import assemble_contig
