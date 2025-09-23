4c478eab0fa92f0a2165fa9f4f311c41812b3866
wis-janusz <wis.janusz@gmail.com>
2025-09-23 22:12:48 +0200

    feat: add mapping by ambiguous strings

--- test/big_data_tests/test_map_big_data.py
+++ test/big_data_tests/test_map_big_data.py
@@ -1,12 +1,21 @@
 import pytest
 import shutil
 from portek.portek_map import MappingPipeline
+from portek.portek_map_str import MappingPipelineString
 
 
 class TestMapBigData:
-    def test_map_by_ambi(self, benchmark):
+    def test_map_bit_kmers(self, benchmark):
         def index_and_mapping_by_ambi():
             mapping_pipeline = MappingPipeline("projects/HIV_M_DNA", 15)
+            mapping_pipeline.index_ref_seq(2, True)
+            mapping_pipeline.run_mapping_by_ambi(2, True)
+
+        benchmark(index_and_mapping_by_ambi)
+
+    def test_map_str_kmers(self, benchmark):
+        def index_and_mapping_by_ambi():
+            mapping_pipeline = MappingPipelineString("projects/HIV_M_DNA", 15)
             mapping_pipeline.index_ref_seq(2, True)
             mapping_pipeline.run_mapping_by_ambi(2, True)
 
