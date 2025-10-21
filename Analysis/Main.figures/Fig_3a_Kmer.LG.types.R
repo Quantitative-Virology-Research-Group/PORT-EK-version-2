d837005d2ce3b6e231bac926865a4e07bb135d95
Quantitative-Virology-Research-Group <bhive.laboratory@gmail.com>
2025-10-21 11:21:23 +0200

    Create Fig_3a_Kmer.LG.types.R

---
+++ Analysis/Main.figures/Fig_3a_Kmer.LG.types.R
@@ -0,0 +1,113 @@
+Logistic.Regression.Kmer.average.count <- function(df) {
+  # 1. feature selection
+  df.kmer.type <- df %>% dplyr::select(kmer.type, kmer.average.count, RMSE, kmer.weight) 
+  df.kmer.type$kmer.type <- as.factor(df.kmer.type$kmer.type)
+  df.kmer.type$kmer.averaage.count <- as.numeric(df.kmer.type$kmer.average.count)
+  df.kmer.type$RMSE <- as.numeric(df.kmer.type$RMSE)
+  df.kmer.type$kmer.weight <- as.numeric(df.kmer.type$kmer.weight)
+  
+  df.kmer.type.b <- sample_n(df.kmer.type, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.kmer.type.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.kmer.type.b[Sample,]
+  test <- df.kmer.type.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- glm(kmer.type ~ kmer.average.count, na.action = na.exclude, family = "binomial", data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test, type = "response")
+  
+  # 5. create roc curve
+  roc <- multiclass.roc(test$kmer.type, prediction)
+  
+  auc <- auc(roc)
+  
+  return(auc)
+}
+
+Logistic.Regression.Kmer.RMSE <- function(df) {
+  # 1. feature selection
+  df.kmer.type <- df %>% dplyr::select(kmer.type, kmer.average.count, RMSE, kmer.weight) 
+  df.kmer.type$kmer.type <- as.factor(df.kmer.type$kmer.type)
+  df.kmer.type$kmer.averaage.count <- as.numeric(df.kmer.type$kmer.average.count)
+  df.kmer.type$RMSE <- as.numeric(df.kmer.type$RMSE)
+  df.kmer.type$kmer.weight <- as.numeric(df.kmer.type$kmer.weight)
+  
+  df.kmer.type.b <- sample_n(df.kmer.type, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.kmer.type.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.kmer.type.b[Sample,]
+  test <- df.kmer.type.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- glm(kmer.type ~ RMSE, na.action = na.exclude, family = "binomial", data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test, type = "response")
+  
+  # 5. create roc curve
+  roc <- multiclass.roc(test$kmer.type, prediction)
+  
+  auc <- auc(roc)
+  
+  return(auc)
+}
+
+Logistic.Regression.Kmer.weight <- function(df) {
+  # 1. feature selection
+  df.kmer.type <- df %>% dplyr::select(kmer.type, kmer.average.count, RMSE, kmer.weight) 
+  df.kmer.type$kmer.type <- as.factor(df.kmer.type$kmer.type)
+  df.kmer.type$kmer.averaage.count <- as.numeric(df.kmer.type$kmer.average.count)
+  df.kmer.type$RMSE <- as.numeric(df.kmer.type$RMSE)
+  df.kmer.type$kmer.weight <- as.numeric(df.kmer.type$kmer.weight)
+  
+  df.kmer.type.b <- sample_n(df.kmer.type, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.kmer.type.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.kmer.type.b[Sample,]
+  test <- df.kmer.type.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- glm(kmer.type ~ kmer.weight, na.action = na.exclude, family = "binomial", data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test, type = "response")
+  
+  # 5. create roc curve
+  roc <- multiclass.roc(test$kmer.type, prediction)
+  
+  auc <- auc(roc)
+  
+  return(auc)
+}
+
+Logistic.Regression.Kmer.count <- function(df) {
+  # 1. feature selection
+  df.kmer.type <- df %>% dplyr::select(kmer.type, kmer.average.count, RMSE, kmer.weight) 
+  df.kmer.type$kmer.type <- as.factor(df.kmer.type$kmer.type)
+  df.kmer.type$kmer.kmer.count <- as.numeric(df.kmer.type$kmer.count)
+  
+  df.kmer.type.b <- sample_n(df.kmer.type, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.kmer.type.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.kmer.type.b[Sample,]
+  test <- df.kmer.type.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- glm(kmer.type ~ kmer.count, na.action = na.exclude, family = "binomial", data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test, type = "response")
+  
+  # 5. create roc curve
+  roc <- multiclass.roc(test$kmer.type, prediction)
+  
+  auc <- auc(roc)
+  
+  return(auc)
+}
