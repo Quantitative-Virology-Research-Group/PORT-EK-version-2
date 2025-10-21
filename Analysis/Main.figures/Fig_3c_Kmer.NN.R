1b5c8106dee0ecca0979d442c23a9fe28c78b440
Quantitative-Virology-Research-Group <bhive.laboratory@gmail.com>
2025-10-21 11:22:21 +0200

    Create Fig_3c_Kmer.NN.R

---
+++ Analysis/Main.figures/Fig_3c_Kmer.NN.R
@@ -0,0 +1,125 @@
+Neural.Network.subtype.kmer.average.count <- function(df) {
+  # 1. feature selection
+  df.subtype <- df %>% dplyr::select(group, kmer.average.count, RMSE, kmer.weight) 
+  df.subtype$group <- as.factor(df.subtype$group)
+  df.subtype$kmer.average.count <- as.numeric(df.subtype$kmer.average.count)
+  df.subtype$RMSE <- as.numeric(df.subtype$RMSE)
+  df.subtype$kmer.weight <- as.numeric(df.subtype$kmer.weight)
+  
+  df.subtype.b <- sample_n(df.subtype, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.subtype.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.subtype.b[Sample,]
+  test <- df.subtype.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- neuralnet::neuralnet(group ~ kmer.average.count, data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test)
+  
+  # 5. calculate accuracy
+  ## To check the accuracy, we have to first convert actual categorical values  into numerical ones and compare them with predicted values. As a result, we will receive a list of boolean values. 
+  
+  ##We can use the `sum` function to find the number of `TRUE` values and divide it  by the total number of samples to get the accuracy. 
+  
+  check = as.numeric(test$group) == max.col(prediction)
+  accuracy = as.numeric(sum(check)/nrow(test))*100
+  
+  return(accuracy)
+}
+
+Neural.Network.subtype.kmer.RMSE <- function(df) {
+  # 1. feature selection
+  df.subtype <- df %>% dplyr::select(group, kmer.average.count, RMSE, kmer.weight) 
+  df.subtype$group <- as.factor(df.subtype$group)
+  df.subtype$kmer.average.count <- as.numeric(df.subtype$kmer.average.count)
+  df.subtype$RMSE <- as.numeric(df.subtype$RMSE)
+  df.subtype$kmer.weight <- as.numeric(df.subtype$kmer.weight)
+  
+  df.subtype.b <- sample_n(df.subtype, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.subtype.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.subtype.b[Sample,]
+  test <- df.subtype.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- neuralnet::neuralnet(group ~ RMSE, data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test)
+  
+  # 5. calculate accuracy
+  ## To check the accuracy, we have to first convert actual categorical values  into numerical ones and compare them with predicted values. As a result, we will receive a list of boolean values. 
+  
+  ##We can use the `sum` function to find the number of `TRUE` values and divide it  by the total number of samples to get the accuracy. 
+  
+  check = as.numeric(test$group) == max.col(prediction)
+  accuracy = as.numeric(sum(check)/nrow(test))*100
+  
+  return(accuracy)
+}
+
+Neural.Network.subtype.kmer.weight <- function(df) {
+  # 1. feature selection
+  df.subtype <- df %>% dplyr::select(group, kmer.average.count, RMSE, kmer.weight) 
+  df.subtype$group <- as.factor(df.subtype$group)
+  df.subtype$kmer.average.count <- as.numeric(df.subtype$kmer.average.count)
+  df.subtype$RMSE <- as.numeric(df.subtype$RMSE)
+  df.subtype$kmer.weight <- as.numeric(df.subtype$kmer.weight)
+  
+  df.subtype.b <- sample_n(df.subtype, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.subtype.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.subtype.b[Sample,]
+  test <- df.subtype.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- neuralnet::neuralnet(group ~ kmer.weight, data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test)
+  
+  # 5. calculate accuracy
+  ## To check the accuracy, we have to first convert actual categorical values  into numerical ones and compare them with predicted values. As a result, we will receive a list of boolean values. 
+  
+  ##We can use the `sum` function to find the number of `TRUE` values and divide it  by the total number of samples to get the accuracy. 
+  
+  check = as.numeric(test$group) == max.col(prediction)
+  accuracy = as.numeric(sum(check)/nrow(test))*100
+  
+  return(accuracy)
+}
+
+Neural.Network.subtype.kmer.count <- function(df) {
+  # 1. feature selection
+  df.subtype <- df %>% dplyr::select(group, kmer.count) 
+  df.subtype$group <- as.factor(df.subtype$group)
+  df.subtype$kmer.count <- as.numeric(df.subtype$kmer.count)
+  
+  df.subtype.b <- sample_n(df.subtype, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.subtype.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.subtype.b[Sample,]
+  test <- df.subtype.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- neuralnet::neuralnet(group ~ kmer.count, data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test)
+  
+  # 5. calculate accuracy
+  ## To check the accuracy, we have to first convert actual categorical values  into numerical ones and compare them with predicted values. As a result, we will receive a list of boolean values. 
+  
+  ##We can use the `sum` function to find the number of `TRUE` values and divide it  by the total number of samples to get the accuracy. 
+  
+  check = as.numeric(test$group) == max.col(prediction)
+  accuracy = as.numeric(sum(check)/nrow(test))*100
+  
+  return(accuracy)
+}
