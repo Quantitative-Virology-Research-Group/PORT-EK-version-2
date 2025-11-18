06a6f531654a5a48f5aa5cf9735f1cd5159f3265
Quantitative-Virology-Research-Group <bhive.laboratory@gmail.com>
2025-11-18 12:52:08 +0100

    Update Fig_4a_Kmer.LG.types.R

--- Analysis/Main.figures/Fig_4a_Kmer.LG.types.R
+++ Analysis/Main.figures/Fig_4a_Kmer.LG.types.R
@@ -111,3 +111,32 @@ Logistic.Regression.Kmer.count <- function(df) {
   
   return(auc)
 }
+
+Logistic.Regression.subtype.kmer.count <- function(df.dna, df.rna) {
+  # 1. feature selection
+  df <- dplyr::bind_rows(df.dna, df.rna)
+  
+  df.kmer.type <- df %>% dplyr::select(type, kmer.count) 
+  df.kmer.type$type <- as.factor(df.kmer.type$type)
+  df.kmer.type$kmer.count <- as.numeric(df.kmer.type$kmer.count)
+  
+  df.kmer.type.b <- sample_n(df.kmer.type, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.kmer.type.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.kmer.type.b[Sample,]
+  test <- df.kmer.type.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- glm(type ~ kmer.count, na.action = na.exclude, family = "binomial", data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test, type = "response")
+  
+  # 5. create roc curve
+  roc <- multiclass.roc(test$type, prediction)
+  
+  auc <- auc(roc)
+  
+  return(auc)
+}
