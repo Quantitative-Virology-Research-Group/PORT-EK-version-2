4b3c4209ee203631a6f83eb44c0b46d6c3de16bb
Quantitative-Virology-Research-Group <bhive.laboratory@gmail.com>
2025-11-18 12:53:31 +0100

    Update Fig_4c_Kmer.NN.R

--- Analysis/Main.figures/Fig_4c_Kmer.NN.R
+++ Analysis/Main.figures/Fig_4c_Kmer.NN.R
@@ -123,3 +123,29 @@ Neural.Network.subtype.kmer.count <- function(df) {
   
   return(accuracy)
 }
+
+Neural.Network.sub.subtype.kmer.count <- function(df) {
+  # 1. feature selection
+  df.subtype <- df %>% dplyr::select(subtype, kmer.count) 
+  df.subtype$subtype <- as.factor(df.subtype$subtype)
+  df.subtype$kmer.count <- as.numeric(df.subtype$kmer.count)
+
+  df.subtype.b <- sample_n(df.subtype, 1000, replace = T)
+  
+  # 2. create training and test samples
+  Sample <- sample(c(TRUE, FALSE), nrow(df.subtype.b), replace = TRUE, prob = c(0.8,0.2))
+  train <- df.subtype.b[Sample,]
+  test <- df.subtype.b[!Sample,]
+  
+  # 3. fit the LR model
+  model <- neuralnet::neuralnet(subtype ~ kmer.count, data = train)
+  
+  # 4. prediction
+  prediction <- predict(model, test)
+  
+  # 5. calculate accuracy
+  check = as.numeric(test$group) == max.col(prediction)
+  accuracy = as.numeric(sum(check)/nrow(test))*100
+  
+  return(accuracy)
+}
